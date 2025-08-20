145d3f347b3d434749ce62f9b37dfebb08e7cb0f
suhyeong <inttype96@gmail.com>
2025-08-20 20:50:14 +0900

    build: backend 환경설정

---
+++ apps/backend/src/main/java/com/solsol/heycalendar/BackendApplication.java
@@ -0,0 +1,14 @@
+package com.solsol.heycalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeycalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeycalendarApplication.class, args);
+	}
+
+
+}
